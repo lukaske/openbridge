e9217f60a51d473b6c029598b23c062912e1ff41
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-02-17 00:16:27 +0100

    upda

--- openbridge/openbridge/settings.py
+++ openbridge/openbridge/settings.py
@@ -71,7 +71,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'openbridge.urls'
